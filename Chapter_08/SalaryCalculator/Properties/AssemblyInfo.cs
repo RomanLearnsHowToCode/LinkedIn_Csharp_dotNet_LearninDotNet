e391b7f44136a5afaab2511906844a40e23517b1
Roman <roman.liewehr@hotmail.com>
2022-02-13 15:13:48 +0000

    Salary Calculator Test Unit created

---
+++ Chapter_08/SalaryCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SalaryCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SalaryCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d3d979d-dcde-4d1c-b4bd-a5f3dcf57b13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Chapter_08/SalaryCalculator/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SalaryCalculator
+{
+    /* REQUIREMENTS REFERENCE
+     To get hourly, divide annual salary by 2080
+    $100,006.40 / 2080 = $48.08 hr
+
+    To get annual, multiply hourly by 2080
+    $48.08 * 2080 = $100,006.40
+     */
+
+    /*
+    RED 
+    GREEN
+    REFACTOR 
+     */
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
