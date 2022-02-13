cd529805929031d8036c29a7af768ff406956d00
Roman <roman.liewehr@hotmail.com>
2022-02-13 14:59:54 +0000

    some C# nameOf feature explanation and sample

--- Chapter_07/nameOfSample/Program.cs
+++ Chapter_07/nameOfSample/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace nameOfSample
 {
     class Program
     {
+        private static List<string> theCrowd;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  Test_NameOf();
+
         }
+
+        ///  <summary>
+        ///  nameOf
+        ///  </summary>
+        ///  <param name=nameof(theCrowd)></param>
+        ///  
+
+        // nameOf operator works within scope and doesn't work in comment
+        private static void Test_NameOf()
+        {
+            theCrowd = null;
+
+            if (theCrowd == null)
+            {
+                throw new ArgumentNullException
+                    (nameof(theCrowd), "The parameter passed null.");
+            };
+
+            foreach (var name in theCrowd)
+            {
+                // Do Something
+            }
+        }
+
     }
 }
