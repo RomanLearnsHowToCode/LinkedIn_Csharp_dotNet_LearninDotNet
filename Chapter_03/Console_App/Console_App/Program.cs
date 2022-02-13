9362e003f7dc87cf7984fecb87f32a275effcb09
Roman <roman.liewehr@hotmail.com>
2022-02-13 09:15:26 +0000

    Chapter_03 - Console_App

---
+++ Chapter_03/Console_App/Console_App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my First Console App");
+            Console.WriteLine("What is your name?");
+            string response = Console.ReadLine();
+            Console.WriteLine($"Hello {response}!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LinkedIn_Csharp_dotNet_LearninDotNet
 C# and .net course repository
+
+- Following: https://www.linkedin.com/learning/learning-dot-net-programming/ course
+
+* dotNET framework & core installed in Visual Studio.. (4.6.1 & 4.8.0 etc.. )
