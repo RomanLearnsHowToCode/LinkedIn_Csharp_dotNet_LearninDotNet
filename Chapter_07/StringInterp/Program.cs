91af814c3072b7d2932789f7cebb79b0f91debc9
Roman <roman.liewehr@hotmail.com>
2022-02-13 15:04:54 +0000

    string interpolation explained and sample created

--- Chapter_07/StringInterp/Program.cs
+++ Chapter_07/StringInterp/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringInterp
 {
     class Program
     {
+        private static List<string> thePeople;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Test_StringInterpolation();
+        }
+
+        /// <summary>
+        /// String interpolation
+        /// </summary>
+        private static void Test_StringInterpolation()
+        {
+            var city = "Burbank";
+            var state = "CA";
+            var zip = 91505;
+            decimal money = 15;
+
+            //var address_oldSchool = 
+            //    string.Format("\n334 S. Main St., {0}, {1}, {2}\n", city, state, zip);
+
+            // this is the string interpolation - new school
+            var address_newSchool = $"\n334 S. Main St., {city}, {state}, {zip}\n";
+            var moneyOwed =
+                $"Today: {DateTime.Now.ToShortDateString()}, Dollars owed: {money:C}";
+
+            Console.WriteLine(address_newSchool);
+            Console.WriteLine(moneyOwed);
+
         }
     }
 }
