87d3c17cc519ff6505ce5e6ea2ea8f53d4962460
Roman <roman.liewehr@hotmail.com>
2022-02-13 15:01:35 +0000

    String Interpolation project created

---
+++ Chapter_07/StringInterp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringInterp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
